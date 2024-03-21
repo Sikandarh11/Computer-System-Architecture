e17b27075c090e7786e5e2d995cb67ccb1d228c1
Sikander Hussain <138971227+Sikandarh11@users.noreply.github.com>
2024-03-21 13:53:42 +0500

    Update PC.v

--- Single Cycle Processor/PC.v
+++ Single Cycle Processor/PC.v
@@ -1,8 +1,9 @@
-module PC (jump_address, pc_out, jump,   clk, rst); 
+module PC (jump_address, pc,pc_out, jump,   clk, rst); 
+		
 		parameter N = 32; 
 		
 		input clk, rst, jump;
-		input [N-1:0] jump_address;
+		input [N-1:0] jump_address, pc;
 		output reg [N-1:0] pc_out; 
 		
 		always @(posedge clk or posedge rst)
@@ -11,8 +12,10 @@ module PC (jump_address, pc_out, jump,   clk, rst);
 				pc_out <=0; 
 			else begin
 				if (jump) pc_out <= jump_address;
+				
 				else 
-					pc_out <= pc_out + 1; 
-			end 
+					pc_out <= pc + 1; 
+			end // ??? 
 		end
+
 endmodule

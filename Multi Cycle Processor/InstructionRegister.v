37323b0fe9f32d413f49ff643a8bbe767b945ea0
Sikander Hussain <138971227+Sikandarh11@users.noreply.github.com>
2024-05-08 10:40:49 +0500

    Update InstructionRegister.v

--- Multi Cycle Processor/InstructionRegister.v
+++ Multi Cycle Processor/InstructionRegister.v
@@ -1,16 +1,18 @@
 module InstructionRegister (
-    input wire clk,
-    input wire reset,
-    input wire [31:0] instruction,
-    output reg [31:0] IR
+	input wire clk,
+	input wire reset,
+	input IRWrite,
+	input wire [31:0] instruction,
+	output reg [31:0] IR
 );
 
 always @(posedge clk or posedge reset) begin
     if (reset) begin
         IR <= 32'h0;
     end else begin
-        IR <= instruction;
-    end
+	if(IRWrite)
+	        IR <= instruction;
+	end
 end
 
 endmodule

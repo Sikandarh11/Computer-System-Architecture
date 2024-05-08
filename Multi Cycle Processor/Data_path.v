5fa25d4072b3123802b106bfc663cee847791e92
Sikander Hussain <138971227+Sikandarh11@users.noreply.github.com>
2024-05-08 10:40:30 +0500

    Update Data_path.v

--- Multi Cycle Processor/Data_path.v
+++ Multi Cycle Processor/Data_path.v
@@ -1,119 +1,114 @@
-module MC_datapath();
-    	reg clk;
-	wire reset;
-	wire [5:0] opcode;
-	wire zero;
-	wire  IorD;
-	wire  MemWrite;
-	wire  MemRead;
-	wire  MemtoReg;
-	wire  IRWrite;
-	wire  PCSource;
-	wire  RegDst;
-	wire  RegWrite;
-	wire  ALUSrcA;
-	wire [1:0] ALUSrcB;
-	wire PCSel;
-	wire  [1:0] ALUOp;
-	
-	wire [31:0] ALUOutFinal;
-	wire [31:0] IR;
+module Datapath_MC_final();
+	reg clk;
+	reg reset;
+	//Control Signals
+	wire zero, IorD, MemWrite, MemToReg, IRWrite, RegDst;
+	wire RegWrite, ALUSrcA, PCWrite;
+	wire [1:0] ALUSrcB, PCSource, ALUOp;
+
+	//Instruction Register
+	wire[31:0] IR;
+
+	//INITIAL
+	initial 
+	begin
+		clk=1; reset=1;#10 reset=0;
+	end
+
+	//FOR CLOCK
+	always #50 clk=~clk;
+
+ 
+	reg[31:0] pc;
+	wire [31:0]jump = IR[25:0]<<2;
+	wire[31:0] A,B, ALUOutFinal;
 //===========================Control Signals============================================
-CU_Multicycle uut(
-	 .clk(clk),
-	 .reset(reset),
-	 .opcode(opcode),
-	 .zero(zero),
-	.IorD(IorD),
-	.MemWrite(MemWrite),
-	.MemRead(MemRead),
-	.MemtoReg(MemtoReg),
-	.IRWrite(IRWrite),
-	.PCSource(PCSource),
-	.RegDst(RegDst),
-	.RegWrite(RegWrite),
-	.ALUSrcA(ALUSrcA),
-	.ALUSrcB(ALUSrcB),
-	.PCSel(PCSel),
-	.ALUOp(ALUOp));
+	CU_Multicycle uut(
+		.clk(clk),
+		.reset(reset),
+		.opcode(IR[31:26]),
+		.zero(zero),
+		.IorD(IorD),
+		.MemWrite(MemWrite),
+		.MemRead(MemRead),
+		.MemtoReg(MemToReg),
+		.IRWrite(IRWrite),
+		.PCSource(PCSource),
+		.RegDst(RegDst),
+		.RegWrite(RegWrite),
+		.ALUSrcA(ALUSrcA),
+		.ALUSrcB(ALUSrcB),
+		.PCWrite_F(PCWrite),
+		.ALUOp(ALUOp));
 	
-	
-//===========================program counter============================================
-	wire [31:0] pc;
-	wire pcWrite;
-	wire jump = IR[25:0];
-	always @(posedge clk)begin
-		if(pcWrite)begin
-			if(PCSource == 0) 
-				pc = pc+4;
-			else if(PCSource == 1) 
-				pc = ALUOutFinal;
-			else if(PCSource == 2)
-				pc = jump;
+	wire [31:0] memAdd;
+	assign memAdd = IorD ? ALUOutFinal : pc;
+	wire [31:0] data_out;//temporary to get data from data_mem
+
+//===========================Data Memory for IF + DATAR & DATAW============================================
+	data_memF DataMemCall(
+		.address(memAdd),
+		.data_in(),
+		.data_out(data_out),
+		.re(MemRead),
+		.we(MemWrite),
+		.clk(clk));
+
+	//PCWrite mux, but need to change it to the whole alusrc thing
+	always@(posedge clk) begin
+		if(reset)begin
+			pc=0;
+		end
+		if(PCWrite)begin
+			if(PCSource == 2'b00)
+				pc=pc+1;
+			else if(PCSource== 2'b01)
+				pc=6;//pc = branch
+			else if(PCSource==2'b10)
+				pc=jump;
 		end
 	end
-//======================================= DataMemory Call =================================================	
-    
-// Mux IorD for DataMemory Call
-	wire [31:0]memAdd;
-	assign memAdd = IorD ? ALUOutFinal : pc;
-
-//DataMem
-	data_mem DataMemCall(
-        .address(memAdd), 
-        .data_in(data_in),
-        .data_out(data_out),  
-        .we(MemWrite), 
-        .clk(clk)
-    );
 
 
-//========================Instruction Register & MDR=====================
-    	
-//Instruction Register
-	
 	InstructionRegister Instr_reg(
-     	.clk(clk),
-    	.reset(reset),
-    	.instruction(data_out),
-    	.IR(IR)
-	);
-//MDR
+		.clk(clk),
+		.reset(reset),
+		.IRWrite(IRWrite),
+		.instruction(data_out),
+		.IR(IR));
+
 	wire [31:0] MDROut;
+
 	MemoryDataRegister memDataReg(
-    	.clk(clk),
-    	.reset(reset),
-    	.memory_data(data_out),
-    	.MDR(MDROut)
-	);
+		.clk(clk),
+		.reset(reset),
+		.memory_data(data_out),
+		.MDR(MDROut));
 
-//===============================Register File & SignExtend & Shift Left 2 ========================
+	wire [4:0] DestRegAdd;
+	assign DestRegAdd = RegDst ? IR[15:11]: IR[20:16];
 
-//Register File
-  //RegDest Mux
-	wire [5:0] DestRegAdd;
-	assign DestRegAdd = RegDst ? IR[15:11] : IR[20:16];
- //MemtoReg Mux
 	wire [31:0] writeData;
-	assign writeData = MemtoReg ? MDROut : ALUOutFinal;
-// reg_file Call
+	assign writeData = MemToReg ? MDROut :ALUOutFinal;
+
 	wire [31:0] rs;
 	wire [31:0] rt;
 	reg_file regFile(
-		.rdata1(A), 
-		.rdata2(B),
+		.rdata1(rs), 
+		.rdata2(rt),
 		.rs1(IR[25:21]),
 		.rs2(IR[20:16]),
 		.rd(DestRegAdd),
 		.write_data(writeData),
 		.clk(clk),
-		.mem_write(mem_write)
+		.mem_write(RegWrite)
 	);
-// Sign Extension Call
+
+	// Sign Extension Call
 	wire [31:0] signExtended;
 	sign_extend se(
-	.a(IR[15:0]),
-	.b(signExtended)
+		.a(IR[15:0]),
+		.b(signExtended)
 	); 
 
 //Shift Left 2
@@ -141,7 +136,7 @@ CU_Multicycle uut(
 //==================================ALU & ALUOut===============================
  
 	wire [31:0] AluIn1;
-	wire [31:0] AluIn2;
+	reg [31:0] AluIn2;
 	wire [31:0] ALU_result;
  //ALUSrcA Mux
 	assign AluIn1 = ALUSrcA ? A : pc;
@@ -150,39 +145,34 @@ CU_Multicycle uut(
 		if(ALUSrcB == 0)
 			AluIn2 = B;
 		else if(ALUSrcB == 1)
-			AluIn2 = 32'b100;
+			AluIn2 = 32'b001;
 		else if(ALUSrcB == 2)
 			AluIn2 = signExtended;
 		else if(ALUSrcB == 3)
 			AluIn2 = seShift;
 	end
-//ALU Control
-wire [3:0] sel;
-ALUControl controlCal(
-	.opcode(IR[2:5]),
-	.aluOP(ALUOp), 
-	.op(sel));
+	wire [3:0] sel;
+	ALUControlMC controlCal(
+		.opcode(IR[5:2]),
+		.aluOP(ALUOp), 
+		.op(sel));
 
 
- //ALU Call
-	
-	alu calculate(
-  	.ans(ALU_result),
-  	.zero(zero),
-  	.a(AluIn1),
-  	.b(AluIn2),
-	.aluOP(ALUOp),  //2 bit	
-	.sel(sel) //4bti
-);
+	 //ALU Call
+	aluMC calculate(
+	  	.ans(ALU_result),
+	  	.zero(zero),
+	  	.a(AluIn1),
+	  	.b(AluIn2),
+		.op(sel) //4bit 
+		);
 
 
- //ALUOut Call
-	ALUOutput finalfinal(
-    	.clk(clk),
-    	.reset(reset),
-    	.ALU_result(ALU_result),
-    	.ALUout(ALUOutFinal));
+	 //ALUOut Call
+	ALUOutputMC finalfinal(
+	    	.clk(clk),
+	    	.reset(reset),
+	    	.ALU_result(ALU_result),
+	    	.ALUout(ALUOutFinal));
 
 endmodule
-	
-

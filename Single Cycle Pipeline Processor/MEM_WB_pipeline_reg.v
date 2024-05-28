42ba93760dffd2f71c4ee810e65c6d07409fc76a
noorkharal <69594707+noorkharal@users.noreply.github.com>
2024-05-28 16:30:58 +0500

    Update MEM_WB_pipeline_reg.v

--- Single Cycle Pipeline Processor/MEM_WB_pipeline_reg.v
+++ Single Cycle Pipeline Processor/MEM_WB_pipeline_reg.v
@@ -7,8 +7,9 @@ module MEM_WB_pipeline_reg (
     input wire [4:0] rd_address,
     input wire RegWrite,
     input wire MemtoReg,
-    
+    input wire [31:0] data,
     output reg [31:0] alu_data_out,
+    output reg [31:0] data_out,
     output reg [31:0] rd_out,
     output reg [4:0] rd_address_out, 
     output reg RegWrite_out,
@@ -17,14 +18,16 @@ module MEM_WB_pipeline_reg (
 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            alu_data_out <= 32'h0;
-            rd_out <= 32'h0;
-            rd_address_out <= 5'b0; 
-            RegWrite_out <= 1'b0;
-            MemtoReg_out <= 1'b0;
+            alu_data_out = 32'h0;
+            rd_out = 32'h0;
+	    data_out = 32'h0;
+            rd_address_out = 5'b0; 
+            RegWrite_out = 1'b0;
+            MemtoReg_out = 1'b0;
         end else begin
             alu_data_out <= alu_data;
             rd_out <= rd;
+	    data_out <= data;
             rd_address_out <= rd_address;
             RegWrite_out <= RegWrite;
             MemtoReg_out <= MemtoReg;

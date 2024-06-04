c34f31497fe5f88b7924e46b8af4d398ad8f43bd
noorkharal <69594707+noorkharal@users.noreply.github.com>
2024-06-04 10:24:05 +0500

    Update IF_ID_pipeline_reg.v

--- Single Cycle Pipeline Processor/IF_ID_pipeline_reg.v
+++ Single Cycle Pipeline Processor/IF_ID_pipeline_reg.v
@@ -1,21 +1,20 @@
 module IF_ID_pipeline_reg (
     input wire clk,
     input wire reset,
-    input wire [31:0] alu_data, // Alu data +4
-    input wire [31:0] inst_mem_data, // Instruction mem_data
-	
+    input wire enable, 
+    input wire [31:0] alu_data,
+    input wire [31:0] inst_mem_data,
     output reg [31:0] alu_data_out,
     output reg [31:0] inst_mem_data_out
-	);
+);
 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            alu_data_out = 32'h0;
-            inst_mem_data_out = 32'h0;
-        end else begin
-            alu_data_out <= alu_data+1;
+            alu_data_out <= 32'h0;
+            inst_mem_data_out <= 32'h0;
+        end else if (enable) begin
+            alu_data_out <= alu_data;
             inst_mem_data_out <= inst_mem_data;
         end
     end
-
 endmodule

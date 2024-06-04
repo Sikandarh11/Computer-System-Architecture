ed3ea97ea985c23b5cdc6e9279a91ab6baf76ed3
noorkharal <69594707+noorkharal@users.noreply.github.com>
2024-06-04 10:25:13 +0500

    Update PC.v

--- Single Cycle Pipeline Processor/PC.v
+++ Single Cycle Pipeline Processor/PC.v
@@ -1,8 +1,8 @@
 
-module PC_pipeline (jump_address, pc, pc_out, jump,   clk, rst); 
+module PC_pipeline (jump_address, pc,enable, pc_out, jump,   clk, rst); 
 		
 		parameter N = 32; 
-		
+		input enable;
 		input clk, rst, jump;
 		input [N-1:0] jump_address, pc;
 		output reg [N-1:0] pc_out; 
@@ -12,11 +12,13 @@ module PC_pipeline (jump_address, pc, pc_out, jump,   clk, rst);
 			if (rst)
 				pc_out <=0; 
 			else begin
-				if (jump) pc_out <= jump_address;
+				if(enable) begin
+					if (jump) pc_out <= jump_address;
 				
-				else 
-				begin
-					pc_out <= pc+1; 	
+					else 
+					begin
+						pc_out <= pc+1; 	
+					end
 				end
 			end // ??? 
 		end
